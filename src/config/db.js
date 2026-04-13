6748c7c400c55cd7ea162846e3eb54eb189b5c78
Richard Sack <richard.sack@web.de>
2026-04-13 18:01:26 +0200

    added .env for db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
 
-const connectDB = () => {mongoose.connect('mongodb://localhost:27017/swap-db')
+dotenv.config();
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = () => {mongoose.connect(mongoURI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB:', err))
     };
